6eb8b79415f96d448f0cfb5605ead4e186c2bbf6
Garvit Singhal <garvitsinghal305@gmail.com>
2026-04-07 11:59:26 +0530

    Use dynamic host based on NODE_ENV

--- server/index.ts
+++ server/index.ts
@@ -72,9 +72,11 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
+  const host =
+    process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
   server.listen({
     port,
-    host: "127.0.0.1", // Use localhost instead of 0.0.0.0 for Windows compatibility
+    host,
     // reusePort: true, // Remove for Windows compatibility
   }, () => {
     log(`serving on port ${port}`);
